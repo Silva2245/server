70e0caa591d55bde19bae58cd911d321e755316e
Silva2245 <silvaspencer2245@gmail.com>
2024-11-11 15:47:14 +0200

    updated

--- dec.py
+++ dec.py
@@ -1,1 +1,24 @@
 from hashlib import *
+import rsa
+import os
+import sys
+from base64 import *
+
+
+ch = str(input('ENC / DEC : '))
+if ch == 'enc':
+    w = str(input('ENTER WORD : '))
+    h = str(input('ENTER HASH : '))
+    if h == 'rsa' or h == 'aes':
+        print()
+    else:
+        print()
+elif ch == 'dec':
+    c = str(input('ENTER CIPHER : '))
+    h = str(input('ENTER HASH : '))
+    if h == 'rsa' or h == 'aes' or h == 'base64':
+        print()
+    else:
+        print()
+else:
+    print('WRONG ANSWER !')
