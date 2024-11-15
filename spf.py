63738e66605a0123e6d60f6f5f6ca602cb88b8a9
Silva2245 <silvaspencer2245@gmail.com>
2024-11-15 15:24:39 +0200

    updated

---
+++ spf.py
@@ -0,0 +1,15 @@
+from scapy.all import *
+import os
+import sys
+
+def sp():
+    print()
+    
+def spoof():
+    print()
+    
+def restore():
+    print()
+    
+v = str(input('ENTER VICTIM IP ADDRESS : '))
+g = str(input('ENTER GATEWAY : '))
